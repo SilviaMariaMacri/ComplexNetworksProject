682015a2b30246e05feedc03dbdf5700517bf0a5
SilviaMariaMacrì <silviamaria.macri@studio.unibo.it>
2020-12-22 18:38:54 +0100

    caricato GH_subnetwork e entropy_Canonical_solofunzione

---
+++ GH_subnetwork.py
@@ -0,0 +1,91 @@
+#%%
+
+import os 
+import pandas as pd
+import networkx as nx
+
+
+directory = 'C:/Users/silvi/Desktop/Fisica/ComplexNetworks/progetto/ComplexNetworksProject/ComplexNetworksProject'
+#directory= '/home/caterina/Documenti/GitHub/ComplexNetworksProject/data'
+os.chdir(directory) 
+
+
+import Percolation
+import entropy_canonical_solofunzione
+
+
+
+
+NameVirusFile = ['string_interactions_InfluenzaA.tsv','string_interactions_HIV1_553.tsv','string_interactions_SARSCov.tsv','string_interactions_yellowfever.tsv']
+
+
+graphs = []
+for i in range(len(NameVirusFile)):
+	
+	virus = pd.read_csv(NameVirusFile[i], sep="\t", usecols=['node1_external_id','node2_external_id', 'combined_score'])
+	graph_virus = Percolation.HumanGraph(virus)
+	
+	
+	#tolgo nodi corrispondenti a proteine virus
+	for i in range(len(virus)):
+		if virus.iloc[i,0].startswith('9606.')!=True:
+			try:
+				graph_virus.remove_node(virus.iloc[i,0])
+			except  nx.NetworkXError:
+				continue
+			
+
+	graphs.append(graph_virus)	
+
+	print(nx.info(graph_virus))
+			
+
+#%%	
+GH_subnetwork = nx.compose(graphs[0],graphs[1])
+
+for i in range(1,len(graphs)-1):
+	GH_subnetwork = nx.compose(GH_subnetwork,graphs[i+1])
+	
+	
+	
+	
+GV = []
+for i in range(len(NameVirusFile)):
+	
+	virus = pd.read_csv(NameVirusFile[i], sep="\t", usecols=['node1_external_id','node2_external_id', 'combined_score'])
+	GV_single = Percolation.VirusGraph(virus)
+	GV.append(GV_single)
+	
+	
+
+GH_subnetwork_directed = GH_subnetwork.to_directed()
+
+#%%
+
+
+Sc = []
+Ss = []
+for i in range(len(GV)):
+	Gnew = nx.compose(GV[i],GH_subnetwork_directed)
+	pp = nx.to_pandas_adjacency(Gnew,weight='weight')
+	Ss_single = entropy_canonical_solofunzione.entropy_canonical_s(pp)
+	Sc_single = entropy_canonical_solofunzione.entropy_canonical_c(pp)
+	Ss.append(Ss_single)
+	Sc.append(Sc_single)
+	
+	
+
+
+
+
+
+#%% se vuoi calcolare singolarmente
+
+
+Ss= entropy_canonical_solofunzione.entropy_canonical_s(pp)
+Sc = entropy_canonical_solofunzione.entropy_canonical_c(pp)
+
+
+
+
+
