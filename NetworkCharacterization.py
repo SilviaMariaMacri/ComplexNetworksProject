9881711d96eb899842d038d3a6881862a157d2b1
SilviaMariaMacrì <silviamaria.macri@studio.unibo.it>
2021-02-24 18:29:24 +0100

    unico file NetworkCharacterization.py

--- NetworkCharacterization.py
+++ NetworkCharacterization.py
@@ -22,6 +22,9 @@ def FileNames(Prefix, FileType):
 
 
 
+
+
+
 #%%
 
 G = []
@@ -49,8 +52,8 @@ NamesDegree=FileNames('DegreeIn_DegreeOut_','.png')
 for i in range(len(G)):	
 	degreeIN = pd.DataFrame.from_dict(nx.in_degree_centrality(G[i]),orient='index')
 	degreeOUT = pd.DataFrame.from_dict(nx.out_degree_centrality(G[i]),orient='index')
-	#BC = pd.DataFrame.from_dict(nx.betweenness_centrality(G[i]),orient='index')
-	#closeness = pd.DataFrame.from_dict(nx.closeness_centrality(G[i]),orient='index')
+	
+	
 	
 	fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(9,7))
 	ax.scatter(degreeIN.iloc[:][0],degreeOUT.iloc[:][0],marker='o', linewidths=0.00001, label='human')
@@ -147,4 +150,144 @@ for i in range(len(G)):
 	ax.legend(['human','virus'])
 		
 	plt.savefig(NamesCC_degreeOUT[i])
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+#%% divido dataframe degree IN e OUT + istogramma degree IN e OUT
+#    + istogramma degree in e out
+
+for i in range(len(G)):
+	
+	degreeIN = pd.DataFrame.from_dict(nx.in_degree_centrality(G[i]),orient='index')
+	degreeOUT = pd.DataFrame.from_dict(nx.out_degree_centrality(G[i]),orient='index')
+	
+	
+	degreeIN_virus_index = []
+	for j in range(len(degreeIN)):
+		if degreeIN.index[j].startswith('9606.')==True:
+			degreeIN_virus_index.append(degreeIN.index[j])
+		
+	degreeIN_human_index = []
+	for j in range(len(degreeIN)):
+		if degreeIN.index[j].startswith('9606.')!=True:
+			degreeIN_human_index.append(degreeIN.index[j])
+
+
+	degreeIN_virus = degreeIN.drop(degreeIN_virus_index)	
+	degreeIN_human = degreeIN.drop(degreeIN_human_index)
+
+
+
+	degreeOUT_virus_index = []
+	for j in range(len(degreeOUT)):
+		if degreeOUT.index[j].startswith('9606.')==True:
+			degreeOUT_virus_index.append(degreeOUT.index[j])
+		
+	degreeOUT_human_index = []
+	for j in range(len(degreeOUT)):
+		if degreeOUT.index[j].startswith('9606.')!=True:
+			degreeOUT_human_index.append(degreeOUT.index[j])
+
+
+	degreeOUT_virus = degreeOUT.drop(degreeOUT_virus_index)	
+	degreeOUT_human = degreeOUT.drop(degreeOUT_human_index)	
+
+
+
+#	fig, (ax1,ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12,4))
+	
+#	ax1.set_title('human')
+#	ax1.hist(degreeIN_human[0],bins=50,alpha=0.5,label='IN')
+#	ax1.hist(degreeOUT_human[0],bins=50,alpha=0.5,label='OUT')
+#	ax1.legend(ncol=1 ,loc='best', fontsize=12)
+	
+	
+#	ax2.set_title('virus')
+#	ax2.hist(degreeIN_virus[0],bins=50,alpha=0.5,label='IN')
+#	ax2.hist(degreeOUT_virus[0],bins=50,alpha=0.5,label='OUT')
+#	ax2.legend(ncol=1 ,loc='best', fontsize=12)
+	
+
+#  closeness
+
+
+
+#for i in range(len(G)):
+	
+#	degreeIN = pd.DataFrame.from_dict(nx.in_degree_centrality(G[i]),orient='index')
+#	degreeOUT = pd.DataFrame.from_dict(nx.out_degree_centrality(G[i]),orient='index')
+
+	#BC = pd.DataFrame.from_dict(nx.betweenness_centrality(G[i]),orient='index')
+	closeness = pd.DataFrame.from_dict(nx.closeness_centrality(G[i]),orient='index')
+	#eigenvector = pd.DataFrame.from_dict(nx.eigenvector_centrality(G[i]),orient='index')
+	
+	
+	
+
+
+
+	clos_virus_index = []
+	for j in range(len(closeness)):
+		if closeness.index[j].startswith('9606.')==True:
+			clos_virus_index.append(closeness.index[j])
+		
+	clos_human_index = []
+	for j in range(len(closeness)):
+		if closeness.index[j].startswith('9606.')!=True:
+			clos_human_index.append(closeness.index[j])
+
+
+	clos_virus = closeness.drop(clos_virus_index)	
+	clos_human = closeness.drop(clos_human_index)
+
+
+	fig1, (ax1a,ax1b) = plt.subplots(nrows=1, ncols=2, figsize=(12,5))
+	ax1a.scatter(degreeIN_human.iloc[:][0],clos_human.iloc[:][0],marker='o', linewidths=0.00001)
+	ax1a.scatter(degreeIN_virus.iloc[:][0],clos_virus.iloc[:][0],marker='o', linewidths=0.00001)
+	
+	ax1b.scatter(degreeOUT_human.iloc[:][0],clos_human.iloc[:][0],marker='o', linewidths=0.00001)
+	ax1b.scatter(degreeOUT_virus.iloc[:][0],clos_virus.iloc[:][0],marker='o', linewidths=0.00001)
+
+
+
+
+
+
+
+
+
+
+	fig2, ax2 = plt.subplots(nrows=1, ncols=1, figsize=(9,7))
+	ax2.hist(clos_virus[0],bins=50,alpha=0.5,label='virus')
+	ax2.hist(clos_human[0],bins=50,alpha=0.5,label='human')
+	ax2.legend(ncol=1 ,loc='best', fontsize=12)
+
+	#fig3, ax3 = plt.subplots(nrows=1, ncols=1, figsize=(9,7))
+	#ax3.scatter(degreeIN.iloc[:][0],eigenvector.iloc[:][0],marker='o', linewidths=0.00001)
+	#ax3.scatter(degreeOUT.iloc[:][0],eigenvector.iloc[:][0],marker='o', linewidths=0.00001)
+
+	#fig4, ax4 = plt.subplots(nrows=1, ncols=1, figsize=(9,7))
+	#ax4.hist(eigenvector[0],bins=50,alpha=0.5,label='IN')
+
+
+
+
+
+
+
+
 	
